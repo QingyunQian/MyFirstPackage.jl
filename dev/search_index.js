7e4eed8e0ec4c0830e0980f398c6f7ac8c465c01
Documenter.jl <documenter@juliadocs.github.io>
2024-02-28 10:58:31 +0000

    build based on ea2491a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyFirstPackage","category":"page"},{"location":"#MyFirstPackage","page":"Home","title":"MyFirstPackage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyFirstPackage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyFirstPackage]","category":"page"}]
+}
